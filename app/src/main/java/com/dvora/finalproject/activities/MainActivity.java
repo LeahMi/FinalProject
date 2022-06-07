3a4dac1b0598f071824814f2e7d2f481a9ff50b1
student <rachellevi120@gmail.com>
2022-06-07 16:38:52 +0300

    desing profil and change add ingredient

--- app/src/main/java/com/dvora/finalproject/activities/MainActivity.java
+++ app/src/main/java/com/dvora/finalproject/activities/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Repository repo = new Repository();
     private Stack<String> prevTitle = new Stack<>();
     private TextView actionBarTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/dvora/finalproject/fragments/ProfileFragment.java
+++ app/src/main/java/com/dvora/finalproject/fragments/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.dvora.finalproject.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,9 +13,11 @@ import androidx.fragment.app.Fragment;
 
 import com.dvora.finalproject.R;
 import com.dvora.finalproject.Repository;
+import com.dvora.finalproject.activities.Login;
 import com.dvora.finalproject.activities.MainActivity;
 import com.dvora.finalproject.entities.User;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -23,8 +27,9 @@ public class ProfileFragment extends Fragment {
     private Repository repo = new Repository();
     public DatabaseReference ref;
     private TextView Name;
+//    private TextView Mail;
     private String FullName;
-
+    private Button logOut;
     public ProfileFragment() {
 
 
@@ -41,6 +46,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_profile, container, false);
+        logOut = (Button) v.findViewById(R.id.log_out);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
+
         //((MainActivity)getActivity()).changeActionBarTitle("פרופיל",false);
         Name= (TextView) v.findViewById(R.id.texthello);
         Name.setText("שלום "+ FullName);
