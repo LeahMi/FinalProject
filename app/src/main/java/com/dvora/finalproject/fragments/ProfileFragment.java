e99a2f936c3f63871259dd0642cb3a91a42907b5
student <rachellevi120@gmail.com>
2022-06-14 09:00:06 +0300

    fix bugs

--- app/src/main/java/com/dvora/finalproject/fragments/ProfileFragment.java
+++ app/src/main/java/com/dvora/finalproject/fragments/ProfileFragment.java
@@ -4,11 +4,15 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -26,22 +30,28 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static com.dvora.finalproject.FirebaseManager.currentUser;
 
-public class ProfileFragment extends Fragment {
+
+public class ProfileFragment<spinner> extends Fragment {
+
     private Repository repo = new Repository();
     private TextView Name;
     private TextView Mail;
     private Button logOut;
-    private TextView tv_c;
-    boolean[] selectC;
-    ArrayList<Integer> cList= new ArrayList<>();
-    String[] cArray ;
+    private String[] types ;
+//    private TextView tv_c;
+    private String favorite_c;
+//    boolean[] selectC;
+//    ArrayList<Integer> cList= new ArrayList<>();
+//    String[] cArray ;
 
     public ProfileFragment() {
 
@@ -67,17 +77,30 @@ public class ProfileFragment extends Fragment {
                 startActivity(new Intent(getActivity(), Login.class));
             }
         });
+        Spinner spinner =(Spinner) v.findViewById(R.id.spinner_category_favorite);
+        repo.getAllCategories(new Repository.OnSearchAllCategories(){
 
-        repo.getAllCategories(new Repository.OnSearchAllCategories() {
             @Override
             public void onCategoriesFound(List<Category> matches) {
-
-                cArray= new String[matches.size()];
-                selectC = new boolean[cArray.length];
+                types= new String[matches.size()];
                 for(int i=0;i<matches.size();++i)
                 {
-                    cArray[i]=matches.get(i).getName();
+                    types[i]=matches.get(i).getName();
                 }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,types );
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Log.v("item", (String) parent.getItemAtPosition(position));
+                        favorite_c = (String)parent.getItemAtPosition(position);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) { }
+                });
+
+
             }
 
             @Override
@@ -90,91 +113,135 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item , types );
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Log.v("item", (String) parent.getItemAtPosition(position));
+//                favorite_c = (String)parent.getItemAtPosition(position);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) { }
+//        });
 
+//        repo.getAllCategories(new Repository.OnSearchAllCategories() {
+//            @Override
+//            public void onCategoriesFound(List<Category> matches) {
+//
+//                cArray= new String[matches.size()];
+//                selectC = new boolean[cArray.length];
+//                for(int i=0;i<matches.size();++i)
+//                {
+//                    cArray[i]=matches.get(i).getName();
+//                }
+//            }
+//
+//            @Override
+//            public void onNoCategoriesFound(String message) {
+//
+//            }
+//
+//            @Override
+//            public void onExceptionOccurred(Exception e) {
+//
+//            }
+//        });
+//
+//
+//        tv_c = v.findViewById(R.id.tv_name_c);
+//        StringBuilder stringBuilder = new StringBuilder();
+//        tv_c.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder builder = new  AlertDialog.Builder(
+//
+//                        getContext()
+//                );
+//                builder.setTitle("קטגוריה אהובה");
+//                builder.setCancelable(false);
+//                builder.setMultiChoiceItems(cArray, selectC, new DialogInterface.OnMultiChoiceClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+//
+//                      if (b){
+//                          cList.add(i);
+//                          Collections.sort(cList);
+//
+//                      }else {
+////                          if(cList.size()==1)
+////                          {
+//
+//                              selectC[i]=false;
+//                              cList.clear();
+//                              stringBuilder.delete(cList.get(i),cList.get(i));
+//
+//
+////                          }
+////                          else cList.remove(i);
+//                      }
+//                    }
+//                });
+//
+//                builder.setPositiveButton("אישור", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+////                        StringBuilder stringBuilder = new StringBuilder();
+//                        for(int j=0;j<cList.size();j++)
+//                        {
+//                            stringBuilder.append(cArray[cList.get(j)]);
+//
+//                            if(j!=cList.size()-1){
+//
+//                                stringBuilder.append(", ");
+//
+//                            }
+//                        }
+//
+//                        tv_c.setText(stringBuilder.toString());
+//
+//                    }
+//                });
+//
+//                builder.setNegativeButton("בטל", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                        dialogInterface.dismiss();
+//
+//                    }
+//                });
+//
+//                builder.setNeutralButton("נקה הכל", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                        for(int j=0; j<selectC.length;++j){
+//                            selectC[j]=false;
+//                            cList.clear();
+//                            tv_c.setText("");
+//                        }
+//
+//                    }
+//                });
+//
+//                builder.show();
+//
+//
+//            }
+//        });
 
-        tv_c = v.findViewById(R.id.tv_name_c);
-        StringBuilder stringBuilder = new StringBuilder();
-        tv_c.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new  AlertDialog.Builder(
-
-                        getContext()
-                );
-                builder.setTitle("קטגוריה אהובה");
-                builder.setCancelable(false);
-                builder.setMultiChoiceItems(cArray, selectC, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-
-                      if (b){
-                          cList.add(i);
-                          Collections.sort(cList);
-
-                      }else {
-//                          if(cList.size()==1)
-//                          {
-
-                              selectC[i]=false;
-                              cList.clear();
-                              stringBuilder.delete(cList.get(i),cList.get(i));
-
-
-//                          }
-//                          else cList.remove(i);
-                      }
-                    }
-                });
-
-                builder.setPositiveButton("אישור", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-//                        StringBuilder stringBuilder = new StringBuilder();
-                        for(int j=0;j<cList.size();j++)
-                        {
-                            stringBuilder.append(cArray[cList.get(j)]);
-
-                            if(j!=cList.size()-1){
-
-                                stringBuilder.append(", ");
-
-                            }
-                        }
-
-                        tv_c.setText(stringBuilder.toString());
-
-                    }
-                });
-
-                builder.setNegativeButton("בטל", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                        dialogInterface.dismiss();
-
-                    }
-                });
-
-                builder.setNeutralButton("נקה הכל", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                        for(int j=0; j<selectC.length;++j){
-                            selectC[j]=false;
-                            cList.clear();
-                            tv_c.setText("");
-                        }
-
-                    }
-                });
-
-                builder.show();
-
-
-            }
-        });
-
-
+//        public void updateUI(FirebaseUser currentCategory){
+//            DatabaseReference ref= FirebaseDatabase.getInstance().getReference().child(currentCategory.getUid());
+//            Log.d("TAG","currentCategory=========="+currentCategory);
+//            String keyId=currentCategory.getUid();
+//            Log.d("TAG","keyId=========="+keyId);
+//            String in="0";
+//            ref.child("favoritecategory").setValue(favorite_c);
+//
+////        ref.child(keyId).child("Details").setValue(currentUser);
+//        }
         Name= (TextView) v.findViewById(R.id.texthello);
 
         repo.getProfile(new Repository.OnSearchProfile() {
@@ -190,7 +257,7 @@ public class ProfileFragment extends Fragment {
         });
 
         Mail = v.findViewById(R.id.mail);
-        Mail.setText(FirebaseManager.currentUser.getEmail());
+        Mail.setText(currentUser.getEmail());
 
         return v;
     }
